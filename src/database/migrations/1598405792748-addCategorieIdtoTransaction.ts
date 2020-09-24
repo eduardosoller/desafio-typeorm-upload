415a261bbdbf6f0f0859daad67c552a877a45310
Eduardo Soller <eduardosoller@gmail.com>
2020-09-24 20:24:35 -0300

    desafio finalizado

---
+++ src/database/migrations/1598405792748-addCategorieIdtoTransaction.ts
@@ -0,0 +1,37 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableForeignKey,
+  TableColumn,
+} from 'typeorm';
+
+export default class addCategorieIdtoTransaction1598405792748
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        name: 'TransactionCategory',
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transaction', 'TransactionCategory');
+    await queryRunner.dropColumn('category_id', 'transaction');
+  }
+}
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -7,12 +7,28 @@ interface Balance {
   outcome: number;
   total: number;
 }
-
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const { income, outcome } = transactions.reduce(
+      (acc, item) => {
+        switch (item.type) {
+          case 'income':
+            acc.income += Number(item.value);
+            break;
+          case 'outcome':
+            acc.outcome += Number(item.value);
+            break;
+          default:
+            break;
+        }
+        return acc;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 }
-
 export default TransactionsRepository;
